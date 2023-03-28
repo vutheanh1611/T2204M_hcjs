c38f0692bd1042750b8351c1d077b0fc03733ce3
vutheanh1611 <105646367+vutheanh1611@users.noreply.github.com>
2023-03-28 09:36:32 +0700

    Add files via upload

---
+++ các hiệu ứng css/js.js
@@ -0,0 +1,73 @@
+// <div class="number-box">
+//     <li>
+//         <div class="number-left">
+//             <p>Người lớn</p>
+//         </div>
+//         <div class="number-right">
+//             <i class="fa-solid fa-minus"></i>
+//             <span>0</span>
+//             <i class="fa-solid fa-plus"></i>
+//         </div>
+//     </li>
+//     <li>
+//         <div class="number-left">
+//             <p>Trẻ em</p>
+//         </div>
+//         <div class="number-right">
+//             <i class="fa-solid fa-minus"></i>
+//             <span>0</span>
+//             <i class="fa-solid fa-plus"></i>
+//         </div>
+//     </li>
+//     <li>
+//         <div class="number-left">
+//             <p>Trẻ sơ sinh</p>
+//         </div>
+//         <div class="number-right">
+//             <i class="fa-solid fa-minus"></i>
+//             <span>0</span>
+//             <i class="fa-solid fa-plus"></i>
+//         </div>
+//     </li>
+// </div>
+// </div>
+//
+//
+//
+// .number-people{
+//     cursor: pointer;
+// }
+// .number-box{
+//     position: absolute;
+//     top: 100%;
+//     background-color: white;
+//     width: 100%;
+//     background-color: rgba(221,221,221,0.8);
+//
+// }
+// .number-box li{
+//     list-style: none;
+//     display:flex;
+//     justify-content: space-around;
+//     padding: 0 12px;
+//     height: 40px;
+// }
+// .number-box li i{
+//
+//     position: relative !important;
+//     left: 0 !important;
+//
+// }
+// .number-left{
+//     width: 60%;
+// }
+// .number-left p{
+//     line-height: 40px;
+// }
+// .number-right{
+//     width: 40%;
+//     display: flex;
+//     text-align: center;
+//     align-items: center;
+//     justify-content: space-between;
+// }
